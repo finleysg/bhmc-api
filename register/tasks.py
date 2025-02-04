b5df3de0ce0f1197c05a39da5dca2a62f0e822a5
Stuart Finley <finleysg@gmail.com>
2025-02-03 20:04:40 -0600

    Add additional logging to the expired reg cleanup task.

--- register/tasks.py
+++ register/tasks.py
@@ -9,4 +9,8 @@ logger = structlog.getLogger(__name__)
 @shared_task(bind=True)
 def remove_expired_registrations(self):
     logger.info("Scheduled job: remove expired registrations")
-    Registration.objects.remove_expired_registrations()
+    count = Registration.objects.clean_up_expired()
+    if count > 0:
+        logger.info("Expired registrations removed", count=count)
+    else:
+        logger.info("No expired registrations found")

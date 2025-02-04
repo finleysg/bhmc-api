2e7c058910e2c9a59e240588675ae0269d1feca0
Stuart Finley <finleysg@gmail.com>
2025-02-03 19:46:06 -0600

    Forgot to add the payment manager to models.py

--- payments/models.py
+++ payments/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.db.models import DO_NOTHING, CASCADE
 
 from events.models import Event
-from payments.managers import RefundManager
+from payments.managers import RefundManager, PaymentManager
 
 NOTIFICATION_CHOICES = (
     ("A", "Admin"),
@@ -27,6 +27,8 @@ class Payment(models.Model):
     confirmed = models.BooleanField(verbose_name="Confirmed", default=False)
     payment_date = models.DateTimeField(verbose_name="Payment date", auto_now_add=True, null=True)
     confirm_date = models.DateTimeField(verbose_name="Confirm date", blank=True, null=True)
+
+    objects = PaymentManager()
 
     def __str__(self):
         return "Payment {} ({})".format(self.payment_code, self.user.last_name)

e0dd27fd3eec8a72ac37292b7c90752153221980
Stuart Finley <finleysg@gmail.com>
2025-12-14 14:50:17 -0600

    Djoser now provides a base email class directly.

--- core/email.py
+++ core/email.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.tokens import default_token_generator
-from templated_mail.mail import BaseEmailMessage
 from djoser import utils
 from djoser.conf import settings
+from djoser.email import BaseEmailMessage
 
 
 class ActivationEmail(BaseEmailMessage):
@@ -18,10 +18,6 @@ class ActivationEmail(BaseEmailMessage):
         return context
 
 
-# class ConfirmationEmail(BaseEmailMessage):
-#     template_name = "email/confirmation.html"
-
-
 class PasswordResetEmail(BaseEmailMessage):
     template_name = "djoser/password_reset.html"
 
@@ -34,24 +30,3 @@ class PasswordResetEmail(BaseEmailMessage):
         context["token"] = default_token_generator.make_token(user)
         context["url"] = settings.PASSWORD_RESET_CONFIRM_URL.format(**context)
         return context
-
-
-# class PasswordChangedConfirmationEmail(BaseEmailMessage):
-#     template_name = "email/password_changed_confirmation.html"
-#
-#
-# class UsernameChangedConfirmationEmail(BaseEmailMessage):
-#     template_name = "email/username_changed_confirmation.html"
-#
-#
-# class UsernameResetEmail(BaseEmailMessage):
-#     template_name = "email/username_reset.html"
-#
-#     def get_context_data(self):
-#         context = super().get_context_data()
-#
-#         user = context.get("user")
-#         context["uid"] = utils.encode_uid(user.pk)
-#         context["token"] = default_token_generator.make_token(user)
-#         context["url"] = settings.USERNAME_RESET_CONFIRM_URL.format(**context)
-#         return context
